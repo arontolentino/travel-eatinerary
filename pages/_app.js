8341f7b747ad5d69c3b15f44dd2cec1835987016
Aron Tolentino <arontolentino@outlook.com>
2020-05-06 22:39:38 -0400

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../global.css';
+import Router from 'next/router';
+import * as gtag from '../lib/gtag';
 
 export default function MyApp({ Component, pageProps }) {
+	Router.events.on('routeChangeComplete', (url) => gtag.pageview(url));
+
 	return <Component {...pageProps} />;
 }
